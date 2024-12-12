da60c0c7a84656312f7ed9f527269166f548d798
unai5665 <unaiperez5665@gmail.com>
2024-12-12 23:50:52 +0000

    añadida clase HabitWithReminders para representar la relacion entre las tablas Habit y Reminder

--- app/src/main/java/com/example/proyectoandroidfinal/model/HabitWithReminders.kt
+++ app/src/main/java/com/example/proyectoandroidfinal/model/HabitWithReminders.kt
@@ -1,2 +1,14 @@
 package com.example.proyectoandroidfinal.model
 
+import androidx.room.Embedded
+import androidx.room.Relation
+
+// Clase para representar la relación entre Habit y Reminder
+data class HabitWithReminders(
+    @Embedded val habit: Habit,  // La entidad Habit
+    @Relation(
+        parentColumn = "id",  // La columna 'id' de Habit (clave primaria)
+        entityColumn = "habitId"  // La columna 'habitId' de Reminder (clave foránea)
+    )
+    val reminders: List<Reminder>  // La lista de recordatorios relacionados con este hábito
+)
