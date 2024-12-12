421686b33acbfcdbefb2a16101ef025d18a0f033
unai5665 <unaiperez5665@gmail.com>
2024-12-12 23:15:47 +0000

    declaradas variables para obtener una instacia de la base de datos y los datos de todas las tablas a traves del Dao

--- app/src/main/java/com/example/proyectoandroidfinal/view/ViewModel.kt
+++ app/src/main/java/com/example/proyectoandroidfinal/view/ViewModel.kt
@@ -1,2 +1,18 @@
 package com.example.proyectoandroidfinal.view
 
+import android.app.*
+import androidx.lifecycle.*
+import com.example.proyectoandroidfinal.model.AppDatabase
+import com.example.proyectoandroidfinal.model.Habit
+import com.example.proyectoandroidfinal.model.Reminder
+import kotlinx.coroutines.*
+
+class HabitViewModel(application: Application) : AndroidViewModel(application) {
+    private val db = AppDatabase.getDatabase(application.applicationContext)
+    private val habitDao = db.habitDao()
+    private val progressDao = db.progressDao()
+    private val reminderDao = db.reminderDao()
+
+
+
+}
