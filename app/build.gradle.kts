b3ea17207a31bacebde55283ac028f770ea9d2fb
unai5665 <unaiperez5665@gmail.com>
2024-12-12 22:06:48 +0000

    añadida dependencias para usar los Mapas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,10 @@ dependencies {
     // WorkManager (para usar notificaciones)
     implementation (libs.androidx.work.runtime.ktx)
 
+    //Mapas
+    implementation(libs.osmdroid.android)
+    implementation(libs.osm.android.compose)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
