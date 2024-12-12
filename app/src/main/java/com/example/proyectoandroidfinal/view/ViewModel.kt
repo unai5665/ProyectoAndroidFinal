2645974e8d3271b87f2fd9a570ad904e130f2ec0
unai5665 <unaiperez5665@gmail.com>
2024-12-12 23:16:53 +0000

    añadidas dos variables para mantener los datos de Habits y Reminders

--- app/src/main/java/com/example/proyectoandroidfinal/view/ViewModel.kt
+++ app/src/main/java/com/example/proyectoandroidfinal/view/ViewModel.kt
@@ -13,6 +13,12 @@ class HabitViewModel(application: Application) : AndroidViewModel(application) {
     private val progressDao = db.progressDao()
     private val reminderDao = db.reminderDao()
 
+    // LiveData para mantener los datos
+    private val _habits = MutableLiveData<List<Habit>>()
+    val habits: LiveData<List<Habit>> = _habits
+
+    private val _reminders = MutableLiveData<List<Reminder>>()
+    val reminders: LiveData<List<Reminder>> = _reminders
 
 
 }
