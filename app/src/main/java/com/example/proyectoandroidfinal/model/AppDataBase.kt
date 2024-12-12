4568666333281b18daf0411d2293b8af9f156086
unai5665 <unaiperez5665@gmail.com>
2024-12-12 23:01:22 +0000

    configurada base de datos

--- app/src/main/java/com/example/proyectoandroidfinal/model/AppDataBase.kt
+++ app/src/main/java/com/example/proyectoandroidfinal/model/AppDataBase.kt
@@ -1,2 +1,28 @@
 package com.example.proyectoandroidfinal.model
 
+import android.content.Context
+import androidx.room.*
+
+@Database(entities = [Habit::class, Progress::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun habitDao(): HabitDao
+    abstract fun progressDao(): ProgressDao
+    abstract fun reminderDao(): ReminderDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "habit_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
