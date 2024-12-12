a0844ee4b7a57b695a2e0ae5b8081bd59353e110
unai5665 <unaiperez5665@gmail.com>
2024-12-12 22:03:15 +0000

    añadida dependencias de Room para usar la base de datos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -40,6 +41,11 @@ android {
 }
 
 dependencies {
+
+    implementation(libs.androidx.room.runtime)
+    implementation (libs.androidx.room.runtime.v252)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
 
     // WorkManager (para usar notificaciones)
     implementation (libs.androidx.work.runtime.ktx)
