afe7089154363c2fc227c43bf1ff959f81dce3e1
unai5665 <unaiperez5665@gmail.com>
2024-12-12 22:11:09 +0000

    añadida tabla Habit

--- app/src/main/java/com/example/proyectoandroidfinal/model/Habits.kt
+++ app/src/main/java/com/example/proyectoandroidfinal/model/Habits.kt
@@ -1,2 +1,13 @@
 package com.example.proyectoandroidfinal.model
 
+import androidx.room.*
+
+@Entity(tableName = "habit")
+data class Habit(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String,
+    val category: String,
+    val frequency: String,
+    val reminderTime: String,
+    val createdAt: Long = System.currentTimeMillis()
+)
