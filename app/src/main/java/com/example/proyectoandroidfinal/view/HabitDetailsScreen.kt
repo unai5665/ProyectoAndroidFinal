aaad9b1d92c1a3619ece0c04d648c39c887e7b5b
unai5665 <unaiperez5665@gmail.com>
2024-12-15 18:41:56 +0000

    añadida funcion HabitDetailsScreen para obtener un habito con su recordatorio

--- app/src/main/java/com/example/proyectoandroidfinal/view/HabitDetailsScreen.kt
+++ app/src/main/java/com/example/proyectoandroidfinal/view/HabitDetailsScreen.kt
@@ -1,2 +1,36 @@
 package com.example.proyectoandroidfinal.view
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.runtime.livedata.observeAsState
+import com.example.proyectoandroidfinal.model.Reminder
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.proyectoandroidfinal.model.HabitWithReminders
+import com.example.proyectoandroidfinal.viewmodel.HabitViewModel
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HabitDetailScreen(navController: NavController, habitId: Int, habitViewModel: HabitViewModel) {
+    val habitWithReminders by habitViewModel.habitsWithReminders.observeAsState(emptyList())
+
+    // Obtener el hábito específico
+    val habitDetail = habitWithReminders.firstOrNull { it.habit.id == habitId }
+
+    if (habitDetail == null) {
+        // Mostrar un mensaje si no se encuentra el hábito
+        Text("Hábito no encontrado", modifier = Modifier.fillMaxSize())
+        return
+    }
+}
+
