c530b440c09405ec7066fb1fd15997aa9d703c92
unai5665 <unaiperez5665@gmail.com>
2024-12-15 10:13:28 +0000

    creada funcion para la MainScreen.kt

--- app/src/main/java/com/example/proyectoandroidfinal/view/MainScreen.kt
+++ app/src/main/java/com/example/proyectoandroidfinal/view/MainScreen.kt
@@ -1,2 +1,27 @@
 package com.example.proyectoandroidfinal.view
 
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.proyectoandroidfinal.model.Habit
+import com.example.proyectoandroidfinal.viewmodel.HabitViewModel
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(navController: NavController, habitViewModel: HabitViewModel) {
+    
+
+
+}
