17cda986646cf3c7ea99a7edfaae86d7551c9112
unai5665 <unaiperez5665@gmail.com>
2024-12-15 09:44:03 +0000

    definida variable para crear y recordar el NavController

--- app/src/main/java/com/example/proyectoandroidfinal/MainActivity.kt
+++ app/src/main/java/com/example/proyectoandroidfinal/MainActivity.kt
@@ -11,37 +11,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
 import com.example.proyectoandroidfinal.ui.theme.ProyectoAndroidFinalTheme
+import com.example.proyectoandroidfinal.view.NavigationGraph
+import com.example.proyectoandroidfinal.viewmodel.HabitViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
+
+
             ProyectoAndroidFinalTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProyectoAndroidFinalTheme {
-        Greeting("Android")
     }
 }
