cb1d002add54278d1468bd47368b80e2c2ff50d7
unai5665 <unaiperez5665@gmail.com>
2024-12-17 09:56:15 +0000

    añadidas variables locales que vamos a utilizar en la pantalla de añadir recordatorios

--- app/src/main/java/com/example/proyectoandroidfinal/view/AddReminderScreen.kt
+++ app/src/main/java/com/example/proyectoandroidfinal/view/AddReminderScreen.kt
@@ -1,2 +1,51 @@
 package com.example.proyectoandroidfinal.view
 
+import android.app.TimePickerDialog
+import android.widget.TimePicker
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.proyectoandroidfinal.model.Habit
+import com.example.proyectoandroidfinal.viewmodel.HabitViewModel
+import java.util.Calendar
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddReminderScreen(
+    navController: NavController,
+    habitViewModel: HabitViewModel
+) {
+    val habitList = habitViewModel.habits.observeAsState(emptyList()).value
+    val selectedHabit = remember { mutableStateOf<Habit?>(null) }
+    val reminderTime = remember { mutableStateOf<Long>(System.currentTimeMillis()) }
+    val reminderMessage = remember { mutableStateOf("") }
+    val expanded = remember { mutableStateOf(false) }
+
+
+}
+
